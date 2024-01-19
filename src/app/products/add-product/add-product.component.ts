7ca898fb27c20e79942a4494d53af9d97194af24
W-nida <nida.w@wowinfobiz.com>
2024-01-19 15:20:04 +0530

    add product

--- src/app/products/add-product/add-product.component.ts
+++ src/app/products/add-product/add-product.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './../../api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,12 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
+  options: any[] = [];
+  selectedOption: any;
+  catoptions: any[] = [];
+  catselectedOption: any;
+  constructor(private apiService: ApiService,) {
 
-  constructor() { }
+  }
   onFileSelected(event) {
     console.log(event.target.files)
   }
   ngOnInit(): void {
+    this.brandData();
+    this.categoryData();
+  }
+  brandData() {
+    this.apiService.getPosts().subscribe(
+      (data: any[]) => {
+        console.log('data', data);
+
+        this.options = data;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+  categoryData() {
+    this.apiService.getCategory().subscribe(
+      (data: any[]) => {
+        console.log('data', data);
+
+        this.catoptions = data;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
   }
 
 }
